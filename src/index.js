eb8d5d16896ec886f723d56a6f47d776ed6062f0
laurakirkham <kirkham.laura@hotmail.com>
2020-02-02 17:20:48 -0500

    added header, inventory, and order components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker.js';
-import './css/style.css'
+import App from './components/App.js';
+import './css/style.css';
 
-render(<StorePicker/>,document.querySelector('#main'));
+    render(<App/>,document.querySelector('#main'));
